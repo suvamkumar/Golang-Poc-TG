ae2247c2ba484cfb0573b28c4c5fc0db24a4a1d6
Shubhamkumar Montee <Shubhamkumar.Montee@rishabh.com>
2020-05-12 13:16:14 -0700

    tiger graph sync with mongo database

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,21 +1,27 @@
 package main
 
-import "crud_with_TG/Golang-Poc-TG/external/services/tigergraph"
+import (
+	app "crud_with_TG/Golang-Poc-TG/cmd/api/myapp"
+	"crud_with_TG/Golang-Poc-TG/external/services/tigergraph"
+	"fmt"
+)
 
 func main() {
-	//	app.StartApplication()
 	connString := tigergraph.TG{ConnectionString: "http://localhost:9000/graph"}
+	connString.SyncDataBaseWithGraph("social", "person", "friendship")
+	fmt.Println("==========")
+	app.StartApplication()
+
 	//connString.GetAllTheVerticesOfAVertex("social", "person")
 	//connString.GetVerticeOfAVertexByID("social", "person", "waqqar")
-	type Person struct {
-		Name   string `json:"name,omitempty"`
-		Gender string `json:"gender,omitempty"`
-		Age    int    `json:"age,omitempty"`
-		State  string `json:"state,omitempty"`
-	}
-	//	p := Person{Name: "ranatunga", Gender: "female", Age: 32, State: "patna"}
+	// type Person struct {
+	// 	Name   string `json:"name,omitempty"`
+	// 	Gender string `json:"gender,omitempty"`
+	// 	Age    int    `json:"age,omitempty"`
+	// 	State  string `json:"state,omitempty"`
+	// }
+	// //	p := Person{Name: "ranatunga", Gender: "female", Age: 32, State: "patna"}
 
-	//	connString.UpsertSingleVertex("social", "person", "ranatunga", p)
-	connString.UpsertSingleEdge("social", "friendship", "shubham", "Rahul")
+	//connString.UpsertSingleEdge("social", "friendship", "shubham", "Rahul")
 
 }
--- cmd/api/myapp/url_mappings.go
+++ cmd/api/myapp/url_mappings.go
@@ -1,6 +1,6 @@
 package app
 
-import user "crud_with_gin_gonic/internal/users/controllers"
+import user "crud_with_TG/Golang-Poc-TG/internal/social/controllers"
 
 func mapUrls() {
 	router.POST("/users", user.Create)
@@ -8,5 +8,6 @@ func mapUrls() {
 	router.DELETE("/users/:id", user.DeleteUser)
 	router.GET("/users/:id", user.GetUser)
 	router.GET("/users", user.GetAllUser)
+	router.GET("/friendship", user.GetAllFriendship)
 	router.GET("/ping", user.Hello)
 }
